23dab7152652269e241c1d322c50a9fb2f335719
zichen2011 <240778488@qq.com>
2014-04-01 13:50:19 +0800

    add a file

---
+++ ProjectFor4SShop/model.h
@@ -0,0 +1,13 @@
+//
+//  model.h
+//  ProjectFor4SShop
+//
+//  Created by lanou3g on 14-4-1.
+//  Copyright (c) 2014年 申闯. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface model : NSObject
+
+@end
---
+++ ProjectFor4SShop/model.m
@@ -0,0 +1,13 @@
+//
+//  model.m
+//  ProjectFor4SShop
+//
+//  Created by lanou3g on 14-4-1.
+//  Copyright (c) 2014年 申闯. All rights reserved.
+//
+
+#import "model.h"
+
+@implementation model
+
+@end
